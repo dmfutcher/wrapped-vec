355b99bb1cdc5a282dc172417780ec2be3084183
David Futcher <david@futcher.io>
2017-11-16 11:12:53 +0000

    Generate all wrapped-vec features from v0.1.x; add documentation attrs & defaults for new/iter/len/is_empty methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![recursion_limit="128"]
+
 extern crate proc_macro;
 extern crate syn;
 #[macro_use]
@@ -29,17 +31,39 @@ impl Idents {
 }
 
 struct Docs {
-    wrapper: String
+    wrapper: String,
+    new: String,
+    is_empty: String,
+    len: String,
+    iter: String
+}
+
+macro_rules! doc_attr {
+    ($ast:ident, $attr:expr, $default:expr) => (
+        attr_string_val($ast, $attr).unwrap_or($default);
+    )
 }
 
 impl Docs {
 
     fn new(ast: &DeriveInput, idents: &Idents) -> Docs {
-        let wrapper = attr_string_val(ast, "CollectionDoc").unwrap_or(format!("A collection of {}s", idents.item));
+        let wrapper = doc_attr!(ast, "CollectionDoc", format!("A collection of {}s", idents.item));
+        let new = doc_attr!(ast, "NewDoc", format!("Creates a new, empty {}", idents.collection));
+        let is_empty = doc_attr!(ast, "IsEmptyDoc", format!("Returns true if the {} contains no {}s", idents.collection, idents.item));
+        let len = doc_attr!(ast, "LenDoc", format!("Returns the number of {}s in the {}", idents.item, idents.collection));
+        let iter = doc_attr!(ast, "IterDoc", format!("Returns an iterator over the {}", idents.collection));
 
         Docs {
-            wrapper: wrapper
+            wrapper: wrapper,
+            new: new,
+            is_empty: is_empty,
+            len: len,
+            iter: iter
         }
+    }
+
+    fn as_parts(&self) -> (&String, &String, &String, &String, &String) {
+        (&self.wrapper, &self.new, &self.is_empty, &self.len, &self.iter)
     }
 
 }
@@ -47,7 +71,11 @@ impl Docs {
 #[proc_macro_derive(WrappedVec, 
     attributes(
         CollectionName,
-        CollectionDoc
+        CollectionDoc,
+        NewDoc,
+        IsEmptyDoc,
+        LenDoc,
+        IterDoc
     )
 )]
 pub fn wrapped_vec(input: TokenStream) -> TokenStream {
@@ -73,12 +101,77 @@ fn impl_wrapped_vec(ast: &DeriveInput) -> Result<quote::Tokens, String> {
 
 fn generate_wrapped_vec(idents: &Idents, docs: &Docs) -> quote::Tokens {
     let (item_ident, collection_ident) = idents.as_parts();
-
-    let collection_doc = &docs.wrapper;
+    let (collection_doc, new_doc, is_empty_doc, len_doc, iter_doc) = docs.as_parts();
 
     quote! {
         #[doc=#collection_doc]
         pub struct #collection_ident(Vec<#item_ident>);
+
+        impl ::std::iter::FromIterator<#item_ident> for #collection_ident {
+            fn from_iter<I: IntoIterator<Item=#item_ident>>(iter: I) -> Self {
+                let mut inner = vec![];
+                inner.extend(iter);
+                #collection_ident(inner)
+            }
+        }
+
+        impl From<Vec<#item_ident>> for #collection_ident {
+
+            fn from(ids: Vec<#item_ident>) -> #collection_ident {
+                let mut new = #collection_ident::new();
+                new.extend(ids);
+                new
+            }
+
+        }
+
+        impl IntoIterator for #collection_ident {
+            type Item = #item_ident;
+            type IntoIter = ::std::vec::IntoIter<#item_ident>;
+
+            fn into_iter(self) -> Self::IntoIter {
+                self.0.into_iter()
+            }
+        }
+
+        impl<'a> IntoIterator for &'a #collection_ident {
+            type Item = &'a #item_ident;
+            type IntoIter = ::std::slice::Iter<'a, #item_ident>;
+
+            fn into_iter(self) -> Self::IntoIter {
+                self.0.iter()
+            }
+        }
+
+        impl Extend<#item_ident> for #collection_ident {
+            fn extend<T: IntoIterator<Item=#item_ident>>(&mut self, iter: T) {
+                self.0.extend(iter);
+            }
+        }
+
+        impl #collection_ident {
+
+            #[doc=#new_doc]
+            pub fn new() -> #collection_ident {
+                #collection_ident(vec![])
+            }
+
+            #[doc=#is_empty_doc]
+            pub fn is_empty(&self) -> bool {
+                self.0.is_empty()
+            }
+
+            #[doc=#len_doc]
+            pub fn len(&self) -> usize {
+                self.0.len()
+            }
+
+            #[doc=#iter_doc]
+            pub fn iter<'a>(&'a self) -> ::std::slice::Iter<'a, #item_ident> {
+                self.into_iter()
+            }
+
+        }
     }
 }
 
