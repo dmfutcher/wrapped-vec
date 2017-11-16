9d7c53be2681259244d590d4414131bf31d5f104
David Futcher <david.futcher@skyscanner.net>
2017-11-16 10:56:26 +0000

    Supporting structs & machinery for passing around idents/docs

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,45 @@ extern crate syn;
 extern crate quote;
 
 use proc_macro::TokenStream;
+use syn::{Ident, DeriveInput};
+
+struct Idents {
+    item: Ident,
+    collection: Ident
+}
+
+impl Idents {
+
+    fn new(ast: &DeriveInput) -> Result<Idents, String> {
+        let collection_name = attr_string_val(ast, "CollectionName").expect("Need [CollectionName=\"...\"]");
+
+        Ok(Idents{
+            item: ast.ident.clone(),
+            collection: Ident::from(collection_name)
+        })
+    }
+
+    fn as_parts(&self) -> (&Ident, &Ident) {
+        (&self.item, &self.collection)
+    }
+
+}
+
+struct Docs {
+    wrapper: String
+}
+
+impl Docs {
+
+    fn new(ast: &DeriveInput, idents: &Idents) -> Docs {
+        let wrapper = attr_string_val(ast, "CollectionDoc").unwrap_or(format!("A collection of {}s", idents.item));
+
+        Docs {
+            wrapper: wrapper
+        }
+    }
+
+}
 
 #[proc_macro_derive(WrappedVec, 
     attributes(
@@ -15,17 +54,27 @@ pub fn wrapped_vec(input: TokenStream) -> TokenStream {
     let s = input.to_string();
     let ast = syn::parse_derive_input(&s).unwrap();
 
-    let gen = impl_wrapped_vec(&ast);
-    gen.parse().unwrap()
+    match impl_wrapped_vec(&ast) {
+        Ok(gen) => {
+            gen.parse().unwrap()
+        },
+        Err(err) => {
+            panic!(err);
+        }
+    }
 }
 
-fn impl_wrapped_vec(ast: &syn::DeriveInput) -> quote::Tokens {
-    let item_ident = &ast.ident;
+fn impl_wrapped_vec(ast: &DeriveInput) -> Result<quote::Tokens, String> {
+    let idents = Idents::new(ast)?;
+    let docs = Docs::new(ast, &idents);
 
-    let collection_name = attr_string_val(ast, "CollectionName").expect("Need [CollectionName=\"...\"]");
-    let collection_ident = syn::Ident::from(collection_name);
+    Ok(generate_wrapped_vec(&idents, &docs))
+}
 
-    let collection_doc = attr_string_val(ast, "CollectionDoc").unwrap_or(format!("A collection of {}s", item_ident));
+fn generate_wrapped_vec(idents: &Idents, docs: &Docs) -> quote::Tokens {
+    let (item_ident, collection_ident) = idents.as_parts();
+
+    let collection_doc = &docs.wrapper;
 
     quote! {
         #[doc=#collection_doc]
